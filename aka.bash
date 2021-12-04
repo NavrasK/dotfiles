ac7be3a9746e7af6f72487f31fa53559834f534c
NavrasK <trailmixerstudios@gmail.com>
2021-12-04 03:17:57 -0700

    Updated bash aliases

--- aka.bash
+++ aka.bash
@@ -1,6 +1,12 @@
 # Bash Alias
 alias vim='nvim'
 
+# ls improved
+alias ls='ls -A -F --color=auto --group-directories-first'
+
+# exa alias for ls
+alias la='exa -1 -l -F -a -h --group-directories-first --no-user --no-time --git'
+
 # Opens emacs in XTerm window (WSL)
 alias emax='
 export DISPLAY=:0.0
