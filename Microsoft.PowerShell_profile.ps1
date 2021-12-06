ec0e4da2632e1ed996bf442bd672eb84f07f22ea
NavrasK <trailmixerstudios@gmail.com>
2021-12-06 02:14:08 -0700

    this is so much easier...

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,57 +1,5 @@
-<#
-$DotfilesLocation = "C:\Users\trail\dotfiles\"
-$BookmarkStorageLocation = "C:\Users\trail\dotfiles\powershellbookmarks.xml"
-$BookmarkBackupLocation = "C:\Users\trail\dotfiles\powershellbookmarks.bkp"
-$Global:BookmarkedDirectories = @{"Un" = "Initialized"}
-
-function UpdateBookmarkFile () {
-	Copy-Item -Path $BookmarkStorageLocation -Destination $BookmarkBackupLocation -Force
-	$Global:BookmarkedDirectories | Export-Clixml -Path $BookmarkStorageLocation
-}
-
-function LoadBookmarkedDirectories () {
-	if (Test-Path -Path $BookmarkStorageLocation -PathType leaf) {
-		Set-Variable -Name BookmarkedDirectories -Value (Import-Clixml -Path $BookmarkStorageLocation)
-		#Write-Host "Found Bookmarks"
-	} else {
-		Set-Variable -Name BookmarkedDirectories -Value @{"dot" = $DotfilesLocation}
-		New-Item -Path $DotfilesLocation -Name "powershellbookmarks.xml"
-		#Write-Host "Bookmarks not found, generating new file"
-		UpdateBookmarkFile
-	}
-}
-
-function ListBookmarkedDirectories () {
-	$Global:BookmarkedDirectories
-}
-
-LoadBookmarkedDirectories
-#>
-
-<#
-function BookmarkDirectory ($Shortcut, $Target = ".") {
-	Write-Host (-Join ("Shortcut: " $($Shortcut) " | Target: " $($Target))) -ForegroundColor blue
-	if (!(Test-Path -Path $Target)) {
-		Write-Host "FAILED: Invalid directory to bookmark" -ForegroundColor red
-		return
-	}
-	if (Test-Path -Path $Target -PathType leaf) {
-		$Target = (Get-Item $Target).Directory.FullName
-	}
-	if ($BookmarkedDirectories.ContainsKey($($Shortcut))) {
-		$response = Read-Host "Bookmark exists, overwrite? (y/n): "
-		if ($response -eq "y") {
-			$BookmarkedDirectories.Add($($Shortcut), $($Target))
-			UpdateBookmarkFile
-		} else {
-			Write-Host (-Join ("ABORTED: " $($Shortcut) " not updated, still points to " $($BookmarkedDirectories[$($Shortcut)]))) -ForegroundColor red
-		}
-	}
-}
-#>
-
-#Set-Alias -Name bd -Value BookmarkDirectory
-#Set-Alias -Name lbd -Value ListBookmarkedDirectories
+# Bookmarks
+$dot = "C:/Users/trail/dotfiles/"
 
 function NVimHereOrThere ($Target = ".") {
 	nvim $Target
@@ -71,5 +19,4 @@ function TouchySubject ($NewFileName) {
 	}
 }
 
-Set-Alias -Name touch -Value TouchySubject
-
+Set-Alias -Name touch -Value TouchySubject
